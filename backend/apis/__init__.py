ac91573ec676e37c1db02a73eb3043252b835978
raghulprashath <raghulprashath@gmail.com>
2022-04-07 23:00:14 +0530

    Docker compose

--- backend/apis/__init__.py
+++ backend/apis/__init__.py
@@ -27,7 +27,7 @@ from apis.models import MyAdminIndexView
 admin = Admin(app,name='Admin Panel',template_mode='bootstrap3',index_view=MyAdminIndexView())
 
 from apis.handleDbms import Roles,Users,Course,Timetable
-from apis.handleDbms import handleDb,getSpecialRights,SpecialRights
+from apis.handleDbms import handleDb,getSpecialRights,SpecialRights,registerUser
 handleDb()
 from apis.handleDbms import selectRoll,checkUserId
 from apis.handleAdmin import Controllers, UsersController, RolesController, spController
