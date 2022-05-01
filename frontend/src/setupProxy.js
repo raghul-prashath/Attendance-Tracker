46b127009648dab874563ee79507552ace2a72d2
raghulprashath <raghulprashath@gmail.com>
2022-05-02 04:38:27 +0530

    Working version 2.2.1

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
+require('dotenv').config();
 
 module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://flaskapi:5000",
+      target: "http://"+ process.env.REACT_HOST + ":5000",
       changeOrigin: true,
     })
   );
